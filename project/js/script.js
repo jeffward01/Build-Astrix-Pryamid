d5359aeef276489c568f8f7724124e9c13b08eb9
Jeff Ward <jeffwardcoding@gmail.com>
2015-07-07 18:19:58 -0400

    fixed base build number
    changed i <= digit to i < digit

--- project/js/script.js
+++ project/js/script.js
@@ -4,7 +4,7 @@ function build() {
   //Grabs digit interger from DOM.  Digit = the number of base rows of Pyramid.
   var digit = +document.getElementById("digit").value;
   //Loop counts from 0 to 'digit' and creates X amoint of rows in the Pyramid.  X = the number of rows, AKA 'digit'.
-  for (var i = 0; i <= digit; i++) {
+  for (var i = 0; i < digit; i++) {
     //Loops from 0 to the current value of I
     for (var x = 0; x <= i; x++) {
       //Creates a * character for each row || for Loop loops through and writes * per line
