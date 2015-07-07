9de39e33b56798acb2d855e47f7978bca325d9f0
Jeff Ward <jeffwardcoding@gmail.com>
2015-07-07 16:40:26 -0400

    fixed output
    Changed document.write to .InnerHTML and styled program

--- project/js/script.js
+++ project/js/script.js
@@ -1,18 +1,34 @@
 function build() {
+  var log = logger('output');
   //Grabs digit interger from DOM.  Digit = the number of base rows of Pyramid.
   var digit = +document.getElementById("digit").value;
   //Opens <center> tag to keep pryamid centered so it is viewed correctly
-  document.write("<center>");
   //Loop counts from 0 to 'digit' and creates X amoint of rows in the Pyramid.  X = the number of rows, AKA 'digit'.
   for (var i = 0; i <= digit; i++) {
     //Loops from 0 to the current value of I
     for (var x = 0; x <= i; x++) {
       //Creates a * character for each row || for Loop loops through and writes * per line
-      document.write("*");
+      log("*");
     }
     //Creates a new line at the end of each iteration (row)
-    document.write("<br/>");
+    log("<br/>");
   }
   //Closes the <center> tag
-  document.write("</center>");
 }
+
+function resetScreen() {
+  document.getElementById('output').innerHTML = '';
+}
+
+function logger(x) {
+ var output = getById(x);
+ return function(text) {
+  output.innerHTML += text +'\n';
+ };
+}
+
+function getById(x){
+ return document.getElementById(x);
+}
+
+
